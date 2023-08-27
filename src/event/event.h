9f6a2825e9f402f036a5865710b704f960e9f723
Viktor Ciolkowski <wikcioo@gmail.com>
2023-08-27 21:41:18 +0200

    Setup event system

---
+++ src/event/event.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <functional>
+
+enum class EventType
+{
+    None = 0,
+    KeyPressed, KeyReleased,
+    MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled,
+    WindowClosed, WindowResized, WindowMinimized, WindowMaximized
+};
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::type; }\
+                               virtual EventType GetEventType() const override { return GetStaticType(); }\
+                               virtual const char* GetName() const override { return #type; }
+
+class Event
+{
+public:
+    virtual EventType GetEventType() const = 0;
+    virtual const char* GetName() const = 0;
+    virtual std::string ToString() const = 0;
+
+protected:
+    bool m_Handled = false;
+};
+
+class EventDispatcher
+{
+    template <typename T>
+    using EventFn = std::function<bool(T&)>;
+public:
+    EventDispatcher(Event& event)
+        : m_Event(event)
+    {
+    }
+
+    template <typename T>
+    bool Dispatch(EventFn<T> callbackFunc)
+    {
+        if (m_Event.GetEventType() == T::GetStaticType())
+        {
+            m_Event.m_Handled = callbackFunc(*(T*)&m_Event);
+            return true;
+        }
+
+        return false;
+    }
+
+private:
+    Event& m_Event;
+};
+
+inline std::ostream& operator<<(std::ostream& os, const Event& event)
+{
+    return os << event.ToString();
+}
---
+++ src/event/window_event.h
@@ -0,0 +1,42 @@
+#include "event.h"
+
+class WindowClosedEvent : public Event
+{
+public:
+    WindowClosedEvent() {}
+
+    EVENT_CLASS_TYPE(WindowClosed);
+};
+
+class WindowResizedEvent : public Event
+{
+public:
+    WindowResizedEvent(unsigned int width, unsigned int height)
+        : m_Width(width), m_Height(height)
+    {
+    }
+
+    inline unsigned int GetWidth() const { return m_Width; }
+    inline unsigned int GetHeight() const { return m_Height; }
+
+    EVENT_CLASS_TYPE(WindowResized);
+
+private:
+    unsigned int m_Width, m_Height;
+};
+
+class WindowMinimizedEvent : public Event
+{
+public:
+    WindowMinimizedEvent() {}
+
+    EVENT_CLASS_TYPE(WindowMinimized);
+};
+
+class WindowMaximizedEvent : public Event
+{
+public:
+    WindowMaximizedEvent() {}
+
+    EVENT_CLASS_TYPE(WindowMaximized);
+};
