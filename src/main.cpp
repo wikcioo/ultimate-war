1f0c400d29c27740bafbbabede124eb2c278aa28
Viktor Ciolkowski <wikcioo@gmail.com>
2023-08-30 21:15:10 +0200

    Remove old event test

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
 #include "core/application.h"
 
-#include "event/event.h"
-#include "event/mouse_event.h"
-
 int main(int argc, char* argv[])
 {
     std::unique_ptr<Application> app = std::make_unique<Application>();
     app->Run();
-
-    auto event = MouseMovedEvent(100, 200);
-    LOG_TRACE("{0}", event.ToString());
 
     return 0;
 }
