88acff6c6531c5823a8918848cdfd40ec2916c53
Viktor Ciolkowski <wikcioo@gmail.com>
2023-09-06 18:50:13 +0200

    Create camera controller
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>

---
+++ src/core/camera_controller.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+
+#include "core/camera.h"
+#include "event/event.h"
+#include "event/mouse_event.h"
+#include "event/window_event.h"
+
+class OrthographicCameraController
+{
+public:
+    OrthographicCameraController(float aspectRatio, bool rotate = false);
+    ~OrthographicCameraController() = default;
+
+    void OnUpdate(float dt);
+    void OnEvent(Event& event);
+
+    inline std::shared_ptr<OrthographicCamera> GetCamera() const { return m_Camera; }
+
+private:
+    bool OnWindowResize(WindowResizedEvent& event);
+    bool OnMouseScrolled(MouseScrolledEvent& event);
+
+private:
+    std::shared_ptr<OrthographicCamera> m_Camera;
+    float m_CameraMovementSpeed = 1.0f;
+    float m_CameraRotationSpeed = 90.0f;
+    bool m_Rotate;
+};
