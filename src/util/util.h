8ab042884658c966879baec1308517f558b52468
Viktor Ciolkowski <wikcioo@gmail.com>
2023-09-08 18:15:01 +0200

    Update utility functions
    Add toggle to keep file extension and extract extension stripping logic into separate function
    
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>

--- src/util/util.h
+++ src/util/util.h
@@ -5,12 +5,22 @@
 class Util
 {
 public:
-    static std::string ExtractFileNameFromPath(const std::string& filepath)
+    static std::string ExtractFileNameFromPath(const std::string& filepath, bool includeExtension = false)
     {
         size_t lastSlash = filepath.find_last_of("/\\");
         lastSlash = lastSlash == std::string::npos ? 0 : lastSlash + 1;
-        size_t lastDot = filepath.find_last_of('.');
-        size_t count = lastDot == std::string::npos ? filepath.size() - lastSlash : lastDot - lastSlash;
-        return filepath.substr(lastSlash, count);
+        std::string fileBaseName = filepath.substr(lastSlash, filepath.length());
+
+        if (includeExtension)
+            return fileBaseName;
+
+        return StripFileExtension(fileBaseName);
+    }
+
+    static std::string StripFileExtension(const std::string& file)
+    {
+        size_t lastDot = file.find_last_of('.');
+        size_t count = lastDot == std::string::npos ? file.length() : lastDot;
+        return file.substr(0, count);
     }
 };
