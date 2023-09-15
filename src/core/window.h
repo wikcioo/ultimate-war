470a16aede992ed8df4ca251350a0a04cec10ef0
Viktor Ciolkowski <wikcioo@gmail.com>
2023-09-15 07:24:34 +0200

    Change default window size
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>

--- src/core/window.h
+++ src/core/window.h
@@ -16,8 +16,8 @@ struct WindowProps
     unsigned int Width, Height;
 
     WindowProps(const std::string& title = "Application",
-                unsigned int width = 1280,
-                unsigned int height = 720)
+                unsigned int width = 1920,
+                unsigned int height = 1080)
         : Title(title), Width(width), Height(height)
     {
     }
