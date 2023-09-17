f7c3bd387710f3d6d3ebafc0abe961de20fdc2ed
Viktor Ciolkowski <wikcioo@gmail.com>
2023-09-17 16:10:17 +0200

    Add boolean uniform setter
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>

--- src/graphics/shader.h
+++ src/graphics/shader.h
@@ -15,6 +15,7 @@ public:
     void Bind() const;
     void Unbind() const;
 
+    void SetBool(const std::string& name, bool value);
     void SetInt(const std::string& name, int value);
     void SetFloat(const std::string& name, float value);
     void SetFloat2(const std::string& name, const glm::vec2& value);
