2ebc997c578eb35e71a46805d157705a0e14958f
Viktor Ciolkowski <wikcioo@gmail.com>
2023-10-05 20:16:10 +0200

    Extract resource loading into separate function

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -18,13 +18,7 @@ Application::Application()
     m_Window = std::make_unique<Window>();
     m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 
-    ResourceManager::LoadShader("color", "assets/shaders/color.glsl");
-    ResourceManager::LoadShader("texture", "assets/shaders/texture.glsl");
-    ResourceManager::LoadTexture("swordsman", "assets/textures/units/swordsman.png");
-    ResourceManager::LoadTexture("archer", "assets/textures/units/archer.png");
-    ResourceManager::LoadTexture("dwarf", "assets/textures/units/dwarf.png");
-    ResourceManager::LoadTexture("demon", "assets/textures/units/demon.png");
-    ResourceManager::LoadTexture("harpy", "assets/textures/units/harpy.png");
+    LoadResources();
 
     Renderer2D::Init();
 
@@ -80,4 +74,20 @@ void Application::Run()
 
         m_Window->OnUpdate();
     }
+}
+
+void Application::LoadResources()
+{
+    ResourceManager::LoadFont("vinque", "assets/fonts/vinque/vinque.otf");
+    ResourceManager::LoadFont("rexlia", "assets/fonts/rexlia/rexlia.otf");
+
+    ResourceManager::LoadShader("font", "assets/shaders/font.glsl");
+    ResourceManager::LoadShader("color", "assets/shaders/color.glsl");
+    ResourceManager::LoadShader("texture", "assets/shaders/texture.glsl");
+
+    ResourceManager::LoadTexture("swordsman", "assets/textures/units/swordsman.png");
+    ResourceManager::LoadTexture("archer", "assets/textures/units/archer.png");
+    ResourceManager::LoadTexture("dwarf", "assets/textures/units/dwarf.png");
+    ResourceManager::LoadTexture("demon", "assets/textures/units/demon.png");
+    ResourceManager::LoadTexture("harpy", "assets/textures/units/harpy.png");
 }
