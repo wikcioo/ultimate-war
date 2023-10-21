05457426085c155cae1ae8d01064a2edf48ce146
Viktor Ciolkowski <wikcioo@gmail.com>
2023-10-21 15:31:45 +0200

    Load new resources

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -85,6 +85,7 @@ void Application::LoadResources()
     ResourceManager::LoadShader("font", "assets/shaders/font.glsl");
     ResourceManager::LoadShader("color", "assets/shaders/color.glsl");
     ResourceManager::LoadShader("texture", "assets/shaders/texture.glsl");
+    ResourceManager::LoadShader("water", "assets/shaders/water.glsl");
 
     ResourceManager::LoadTexture("swordsman", "assets/textures/units/swordsman.png");
     ResourceManager::LoadTexture("archer", "assets/textures/units/archer.png");
@@ -92,12 +93,15 @@ void Application::LoadResources()
     ResourceManager::LoadTexture("demon", "assets/textures/units/demon.png");
     ResourceManager::LoadTexture("harpy", "assets/textures/units/harpy.png");
 
+    ResourceManager::LoadTexture("sand", "assets/textures/envs/sand.png");
+    ResourceManager::LoadTexture("stone", "assets/textures/envs/stone.png");
+    ResourceManager::LoadTexture("tree", "assets/textures/envs/tree.png");
+
     ResourceManager::LoadTexture("workshop", "assets/textures/buildings/workshop.png");
     ResourceManager::LoadTexture("gold_mine", "assets/textures/buildings/gold_mine.png");
     ResourceManager::LoadTexture("harpy_tower", "assets/textures/buildings/harpy_tower.png");
     ResourceManager::LoadTexture("demon_castle", "assets/textures/buildings/demon_castle.png");
     ResourceManager::LoadTexture("dragon_lair", "assets/textures/buildings/dragon_lair.png");
-
 }
 
 void Application::InitializeColors()
