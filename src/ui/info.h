61f9a9714035c6c0ec14e3b110a17581e6250f73
Viktor Ciolkowski <wikcioo@gmail.com>
2023-10-21 16:36:01 +0200

    Replace resource text with textures

--- src/ui/info.h
+++ src/ui/info.h
@@ -4,6 +4,7 @@
 
 #include "game/player_manager.h"
 #include "ui/ui_element.h"
+#include "core/resource_manager.h"
 
 class Info : public UIElement
 {
@@ -17,4 +18,16 @@ public:
 
 private:
     std::shared_ptr<PlayerManager> m_PlayerManager;
+
+    float m_TextScale = 0.3f;
+    float m_ResourceScale = 0.08f;
+    float m_ResourceOffset = 0.1f;
+    float m_ResourceNumberOffset = 0.07f;
+    float m_ResourceStartX = 0.07f;
+    float m_ResourceStartY = 0.08f;
+
+    std::shared_ptr<Texture2D> m_WoodTexture = ResourceManager::GetTexture("wood");
+    std::shared_ptr<Texture2D> m_RockTexture = ResourceManager::GetTexture("rock");
+    std::shared_ptr<Texture2D> m_SteelTexture = ResourceManager::GetTexture("steel");
+    std::shared_ptr<Texture2D> m_GoldTexture = ResourceManager::GetTexture("gold");
 };
