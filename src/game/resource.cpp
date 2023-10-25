969388ddcb5b7c2f9f97ece85930b1cc43accdb4
Viktor Ciolkowski <wikcioo@gmail.com>
2023-10-25 19:07:36 +0200

    Extract common resource data into function

--- src/game/resource.cpp
+++ src/game/resource.cpp
@@ -1,5 +1,8 @@
 #include "resource.h"
 
+#include "game/color_data.h"
+#include "core/resource_manager.h"
+
 Resources& Resources::operator+=(const Resources& other)
 {
     Wood  += other.Wood;
@@ -26,4 +29,26 @@ bool Resources::operator>=(const Resources& other)
         Steel >= other.Steel &&
         Gold  >= other.Gold
     );
+}
+
+ResourceData Resources::GetResourceData()
+{
+    ResourceData data;
+
+    static auto woodTexture = ResourceManager::GetTexture("wood");
+    static auto rockTexture = ResourceManager::GetTexture("rock");
+    static auto steelTexture = ResourceManager::GetTexture("steel");
+    static auto goldTexture = ResourceManager::GetTexture("gold");
+
+    data.ResourceNumberColors[0] = ColorData::Get().Resources.Wood;
+    data.ResourceNumberColors[1] = ColorData::Get().Resources.Rock;
+    data.ResourceNumberColors[2] = ColorData::Get().Resources.Steel;
+    data.ResourceNumberColors[3] = ColorData::Get().Resources.Gold;
+
+    data.ResourceTextures[0] = woodTexture;
+    data.ResourceTextures[1] = rockTexture;
+    data.ResourceTextures[2] = steelTexture;
+    data.ResourceTextures[3] = goldTexture;
+
+    return data;
 }
--- src/game/resource.h
+++ src/game/resource.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <memory>
+
+#include <glm/glm.hpp>
+#include "graphics/texture.h"
+
+struct ResourceData
+{
+    static const int NumResources = 4;
+    glm::vec3 ResourceNumberColors[NumResources];
+    std::shared_ptr<Texture2D> ResourceTextures[NumResources];
+};
+
 struct Resources
 {
     int Wood;
@@ -10,4 +22,6 @@ struct Resources
     Resources& operator+=(const Resources& other);
     Resources& operator-=(const Resources& other);
     bool operator>=(const Resources& other);
+
+    static ResourceData GetResourceData();
 };
