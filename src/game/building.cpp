0b9de4abf3b4111d2913103184366a7af5c420f3
Viktor Ciolkowski <wikcioo@gmail.com>
2023-11-04 00:31:44 +0100

    Add upgrade level and upgrade cost to buildings

--- src/game/building.cpp
+++ src/game/building.cpp
@@ -1,14 +1,14 @@
 #include "building.h"
 
 std::unordered_map<BuildingType, BuildingData> BuildingDataMap = {
-    { BuildingType::TARGET,       { { 40, 30, 25, 45 },       "target" } },
-    { BuildingType::BLACKSMITH,   { { 20, 15, 10,  5 },   "blacksmith" } },
-    { BuildingType::GOLD_MINE,    { { 10, 25, 15, 40 },    "gold_mine" } },
-    { BuildingType::HARPY_TOWER,  { { 25, 20, 10, 35 },  "harpy_tower" } },
-    { BuildingType::DEMON_CASTLE, { { 30, 40, 45, 50 }, "demon_castle" } }
+    { BuildingType::TARGET,       { { 40, 30, 25, 45 }, { 8, 6, 4, 8 },       "target" } },
+    { BuildingType::BLACKSMITH,   { { 20, 15, 10,  5 }, { 4, 2, 2, 2 },   "blacksmith" } },
+    { BuildingType::GOLD_MINE,    { { 10, 25, 15, 40 }, { 2, 4, 2, 8 },    "gold_mine" } },
+    { BuildingType::HARPY_TOWER,  { { 25, 20, 10, 35 }, { 4, 4, 2, 6 },  "harpy_tower" } },
+    { BuildingType::DEMON_CASTLE, { { 30, 40, 45, 50 }, { 6, 8, 8, 9 }, "demon_castle" } }
 };
 
 Building::Building(BuildingType type)
-    : m_Type(type)
+    : m_Type(type), m_Level(0)
 {
 }
