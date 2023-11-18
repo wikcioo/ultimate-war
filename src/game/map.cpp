61d6ff3f76d1632ade345c92da3a1cec161108bf
Viktor Ciolkowski <wikcioo@gmail.com>
2023-11-18 21:50:38 +0100

    Log warning if trying to get non-existent tile

--- src/game/map.cpp
+++ src/game/map.cpp
@@ -1,5 +1,6 @@
 #include "map.h"
 
+#include "core/logger.h"
 #include "game/tile.h"
 
 GameMap::GameMap(MapData& mapData)
@@ -12,6 +13,7 @@ const std::shared_ptr<Tile>& GameMap::GetTile(int x, int y)
     if (x < GetTileCountX() && y < GetTileCountY() && x > -1 && y > -1)
         return m_MapData[y][x];
 
+    LOG_WARN("Map: Tile coords ({0},{1}) out of range", x, y);
     static auto tile = std::make_shared<Tile>(TileEnvironment::NONE, glm::ivec2(x, y));
     return tile;
 }
