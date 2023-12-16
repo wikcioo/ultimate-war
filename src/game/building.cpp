a9c1897b5364e6dab7df0f415a0c4a48d0b27b5c
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-12-16 23:41:22 +0100

    Adjust resource cost and stat values

--- src/game/building.cpp
+++ src/game/building.cpp
@@ -1,11 +1,11 @@
 #include "building.h"
 
 std::unordered_map<BuildingType, BuildingData> BuildingDataMap = {
-    { BuildingType::TARGET,       { { 40, 30, 25, 45 }, { 8, 6, 4, 8 },       "target" } },
-    { BuildingType::BLACKSMITH,   { { 20, 15, 15, 20 }, { 4, 2, 2, 2 },   "blacksmith" } },
-    { BuildingType::GOLD_MINE,    { { 10, 25, 15,  5 }, { 2, 4, 2, 8 },    "gold_mine" } },
-    { BuildingType::HARPY_TOWER,  { { 25, 20, 10, 35 }, { 4, 4, 2, 6 },  "harpy_tower" } },
-    { BuildingType::DEMON_CASTLE, { { 30, 40, 45, 50 }, { 6, 8, 8, 9 }, "demon_castle" } }
+    { BuildingType::TARGET,       { { 30, 30, 25, 25 }, { 2, 4, 4, 3 },       "target" } },
+    { BuildingType::BLACKSMITH,   { { 20, 35, 35, 20 }, { 4, 3, 3, 5 },   "blacksmith" } },
+    { BuildingType::GOLD_MINE,    { { 10, 25, 15, 10 }, { 3, 4, 3, 5 },    "gold_mine" } },
+    { BuildingType::HARPY_TOWER,  { { 25, 20, 10, 35 }, { 4, 4, 3, 6 },  "harpy_tower" } },
+    { BuildingType::DEMON_CASTLE, { { 30, 30, 35, 40 }, { 3, 6, 6, 5 }, "demon_castle" } }
 };
 
 Building::Building(BuildingType type)
